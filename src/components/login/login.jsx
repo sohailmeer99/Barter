115f0c4e553063ea17d16459e838e6bc62b809d2
sohailmeer99 <sohail.752@uswagb.edu.pk>
2025-02-26 18:02:26 +0500

    I create components in src folder and build basic of login form.

---
+++ src/components/login/login.jsx
@@ -0,0 +1,18 @@
+import react from "react"
+function Login(){
+    return(
+        <div>
+            <img src="https://media.baamboozle.com/uploads/images/960138/1677600448_50792.jpeg" />
+            <div>
+            <input />
+            <input />
+            </div>
+            <div>
+            <button>Forget Password</button>
+            <button>Login</button>
+            </div>
+            <button>Create an Account</button>
+        </div>
+    );
+}
+export {Login};
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import {StrictMode} from "react"
+import reactDom from "react-dom/client"
+import App from "./App.jsx"
+reactDom.createRoot(document.getElementById("root")).render(
+    <StrictMode>
     <App />
-  </StrictMode>,
-)
+    </StrictMode>
+);
