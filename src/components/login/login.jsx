67673f89e0606a646cf44be1bd9ae837cfa2c698
sohailmeer99 <sohail.752@uswagb.edu.pk>
2025-03-01 02:26:49 +0500

    Style the form and give it structure only not the styling. I just centre the elements and learn about flex and grid displays

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,23 +1,23 @@
 import react from "react"
 function Login(){
     return(
+        <div className="login_container">
         <div className = "login_form">
         <form>
-            <div>
+            <div className="login_img">
                 <img src="https://media.baamboozle.com/uploads/images/960138/1677600448_50792.jpeg" />
             </div>
-            <div>
+            <div className="login_inputs">
                 <input />
                 <input />
             </div>
-            <div>
+            <div className="login_buttons">
                 <button>Forget Password</button>
                 <button>Login</button>
-            </div>
-            <div>
                 <button>Create an Account</button>
             </div>
             </form>
+        </div>
         </div>
     );
 }
