9da102174ae8c1a42fd45cb56a678958ffdc8c25
sohailmeer99 <sohail.752@uswagb.edu.pk>
2025-03-08 18:41:54 +0500

    i made the guestpage navbar but it is still incomplete

---
+++ src/components/home_page/nav_bar/navbar.jsx
@@ -0,0 +1,31 @@
+import { SearchBar } from "./search_bar";
+function Navbar(){
+    return(
+        <div>
+            <nav className="navbar">
+                <div className="navbar-div">
+                    <div className="navbar-left">
+                        <h1>Barter.</h1>
+                    </div>
+                    <div className="navbar-right">
+                    <div>
+                        <SearchBar />
+                    </div>
+                    <div>
+                        <button className="nav-icon">
+                            <span><i className="fa-solid fa-user" style={{fontSize : "25px", padding : "5px", backgroundColor : "#fff", color : "#3D3D3D"}}></i></span>
+                        </button>
+                    </div>
+                    <div>
+                        <button className="nav-icon">
+                            <span><i className="fa-duotone fa-solid fa-gear" style={{fontSize : "25px", padding : "5px", backgroundColor : "#fff", color : "#3D3D3D"}}></i></span>
+                        </button>
+                    </div>
+                    </div>
+                    
+                </div>
+            </nav>
+        </div>
+    )
+}
+export {Navbar};
---
+++ src/components/home_page/nav_bar/search_bar.jsx
@@ -0,0 +1,8 @@
+function SearchBar(){
+    return(
+        <div>
+            <input className="nav-searchbar"/>
+        </div>
+    )
+}
+export {SearchBar};
