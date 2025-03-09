810b3dfd194f7f7e36c4acb8e1b05a1304b50762
sohailmeer99 <sohail.752@uswagb.edu.pk>
2025-03-09 23:50:51 +0500

    add search icon to search-bar

--- src/components/home_page/nav_bar/search_bar.jsx
+++ src/components/home_page/nav_bar/search_bar.jsx
@@ -1,7 +1,7 @@
 function SearchBar(){
     return(
-        <div>
-            <input className="nav-searchbar"/>
+        <div style={{display:"flex"}}>
+            <input style={{paddingLeft:"10px", fontSize: "1.5rem", color:"#3D3D3D"}} className="nav-searchbar"/><span><i className="fas fa-search" style={{fontSize : "25px", padding : "5px"}}></i></span>
         </div>
     )
 }
