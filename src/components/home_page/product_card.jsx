d0d370679d4aa0b16db577f93df5c534cbc8e34d
sohailmeer99 <sohail.752@uswagb.edu.pk>
2025-03-13 18:11:58 +0500

    I work on Product cards but they are still not complete making many issues in display and design

--- src/components/home_page/product_card.jsx
+++ src/components/home_page/product_card.jsx
@@ -1,7 +1,8 @@
 function ProductCard(){
     return (
-        <div className="product_card">
-            <div>
+        <section className="product_card_section">
+            <div className="product_card">
+            <div className="product_card_img1">
                 <img src="https://images.squarespace-cdn.com/content/v1/5264f7c9e4b0a3247c641860/1632989627837-HOJOKB3ABAMMYYJB5SW3/iphone-13-pro-photography_14.jpg"/>
             </div>
             <div>
@@ -10,7 +11,129 @@ function ProductCard(){
             <div>
                 <h2>Available in <span>Karachi</span></h2>
             </div>
-        </div>
+            </div>
+            <div className="product_card">
+            <div className="product_card_img2">
+                <img src="https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.large.jpg"/>
+            </div>
+            <div>
+                <h1>product Name</h1>
+            </div>
+            <div>
+                <h2>Available in <span>Karachi</span></h2>
+            </div>
+            </div>
+            <div className="product_card">
+            <div className="product_card_img3">
+                <img src="https://i.guim.co.uk/img/media/18badfc0b64b09f917fd14bbe47d73fd92feeb27/189_335_5080_3048/master/5080.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=1562112c7a64da36ae0a5e75075a0d12"/>
+            </div>
+            <div>
+                <h1>product Name</h1>
+            </div>
+            <div>
+                <h2>Available in <span>Karachi</span></h2>
+            </div>
+            </div>
+            <div className="product_card">
+            <div className="product_card_img1">
+                <img src="https://images.squarespace-cdn.com/content/v1/5264f7c9e4b0a3247c641860/1632989627837-HOJOKB3ABAMMYYJB5SW3/iphone-13-pro-photography_14.jpg"/>
+            </div>
+            <div>
+                <h1>product Name</h1>
+            </div>
+            <div>
+                <h2>Available in <span>Karachi</span></h2>
+            </div>
+            </div>
+            <div className="product_card">
+            <div className="product_card_img2">
+                <img src="https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.large.jpg"/>
+            </div>
+            <div>
+                <h1>product Name</h1>
+            </div>
+            <div>
+                <h2>Available in <span>Karachi</span></h2>
+            </div>
+            </div>
+            <div className="product_card">
+            <div className="product_card_img3">
+                <img src="https://i.guim.co.uk/img/media/18badfc0b64b09f917fd14bbe47d73fd92feeb27/189_335_5080_3048/master/5080.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=1562112c7a64da36ae0a5e75075a0d12"/>
+            </div>
+            <div>
+                <h1>product Name</h1>
+            </div>
+            <div>
+                <h2>Available in <span>Karachi</span></h2>
+            </div>
+            </div>
+            <div className="product_card">
+            <div className="product_card_img1">
+                <img src="https://images.squarespace-cdn.com/content/v1/5264f7c9e4b0a3247c641860/1632989627837-HOJOKB3ABAMMYYJB5SW3/iphone-13-pro-photography_14.jpg"/>
+            </div>
+            <div>
+                <h1>product Name</h1>
+            </div>
+            <div>
+                <h2>Available in <span>Karachi</span></h2>
+            </div>
+            </div>
+            <div className="product_card">
+            <div className="product_card_img2">
+                <img src="https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.large.jpg"/>
+            </div>
+            <div>
+                <h1>product Name</h1>
+            </div>
+            <div>
+                <h2>Available in <span>Karachi</span></h2>
+            </div>
+            </div>
+            <div className="product_card">
+            <div className="product_card_img3">
+                <img src="https://i.guim.co.uk/img/media/18badfc0b64b09f917fd14bbe47d73fd92feeb27/189_335_5080_3048/master/5080.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=1562112c7a64da36ae0a5e75075a0d12"/>
+            </div>
+            <div>
+                <h1>product Name</h1>
+            </div>
+            <div>
+                <h2>Available in <span>Karachi</span></h2>
+            </div>
+            </div>
+            <div className="product_card">
+            <div className="product_card_img1">
+                <img src="https://images.squarespace-cdn.com/content/v1/5264f7c9e4b0a3247c641860/1632989627837-HOJOKB3ABAMMYYJB5SW3/iphone-13-pro-photography_14.jpg"/>
+            </div>
+            <div>
+                <h1>product Name</h1>
+            </div>
+            <div>
+                <h2>Available in <span>Karachi</span></h2>
+            </div>
+            </div>
+            <div className="product_card">
+            <div className="product_card_img2">
+                <img src="https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.large.jpg"/>
+            </div>
+            <div>
+                <h1>product Name</h1>
+            </div>
+            <div>
+                <h2>Available in <span>Karachi</span></h2>
+            </div>
+            </div>
+            <div className="product_card">
+            <div className="product_card_img3">
+                <img src="https://i.guim.co.uk/img/media/18badfc0b64b09f917fd14bbe47d73fd92feeb27/189_335_5080_3048/master/5080.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=1562112c7a64da36ae0a5e75075a0d12"/>
+            </div>
+            <div>
+                <h1>product Name</h1>
+            </div>
+            <div>
+                <h2>Available in <span>Karachi</span></h2>
+            </div>
+            </div>
+        </section>
     )
 };
 export {ProductCard};
